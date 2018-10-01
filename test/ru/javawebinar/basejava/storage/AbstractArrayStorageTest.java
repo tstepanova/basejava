807ccbcbbb9fbd39c206db81f344f57c5c91b5b0
tstepanova <tamara.stepanova85@gmail.com>
2018-10-01 09:11:59 +0300

    Synchronization with Lesson05 (Static initializer block and Collections)

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -19,12 +19,20 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
     private static final String UUID_4 = "uuid4";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
-    private static final Resume RESUME_2 = new Resume(UUID_2);
-    private static final Resume RESUME_3 = new Resume(UUID_3);
-    private static final Resume RESUME_4 = new Resume(UUID_4);
-    private static final Resume[] RESUME_ARRAY = {RESUME_1, RESUME_2, RESUME_3};
+    private static final Resume RESUME_1;
+    private static final Resume RESUME_2;
+    private static final Resume RESUME_3;
+    private static final Resume RESUME_4;
+    private static final Resume[] RESUME_ARRAY;
 
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+        RESUME_4 = new Resume(UUID_4);
+		RESUME_ARRAY = new Resume[]{RESUME_1, RESUME_2, RESUME_3};
+    }	
+	
     AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
