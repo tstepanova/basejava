4b1b025630eb38ec26524a68e15ee42d7c122806
tstepanova <tamara.stepanova85@gmail.com>
2018-12-18 11:35:46 +0300

    HW11

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -58,6 +58,47 @@ public class MainConcurrency {
             }
         });
         System.out.println(mainConcurrency.counter);
+
+        final String resource1 = "resource1";
+        final String resource2 = "resource2";
+
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (resource1) {
+                    System.out.println("Thread 1: locked resource 1");
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Thread 1: waiting resource 2");
+                    synchronized (resource2) {
+                        System.out.println("Thread 1: locked resource 2");
+                    }
+                }
+            }
+        };
+        thread1.start();
+
+        Thread thread2 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (resource2) {
+                    System.out.println("Thread 2: locked resource 2");
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Thread 2: waiting resource 1");
+                    synchronized (resource1) {
+                        System.out.println("Thread 2: locked resource 1");
+                    }
+                }
+            }
+        };
+        thread2.start();
     }
 
     private synchronized void inc() {
