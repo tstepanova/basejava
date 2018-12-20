5bd3e03654f97e4a596dcf6e76378c559fb9d6cb
tstepanova <tamara.stepanova85@gmail.com>
2018-12-20 09:09:11 +0300

    HW12 optimization

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -17,16 +17,13 @@ public class MainStreams {
     }
 
     private static int minValue(int[] values) {
-        return Integer.parseInt(IntStream.of(values).mapToObj(Integer::toString).sorted().distinct().collect(Collectors.joining()));
+        return IntStream.of(values).sorted().distinct().reduce(0, (x, y) -> x * 10 + y);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         Integer sum = integers.stream().reduce((s1, s2) -> s1 + s2).orElse(0);
-        Map<Boolean, List<Integer>> res = integers.stream().collect(Collectors.partitioningBy((p) -> (p % 2) == 0));
-        if ((sum % 2) == 0) {
-            return res.get(false);
-        }
-        return res.get(true);
+        Map<Boolean, List<Integer>> map = integers.stream().collect(Collectors.partitioningBy((p) -> (p % 2) == 0));
+        return map.get((sum % 2) != 0);
     }
 
 }
