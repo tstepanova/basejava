5dd108f1cd565af85544784858193985a7369ec9
tstepanova <tamara.stepanova85@gmail.com>
2018-12-20 14:04:23 +0300

    HW12 stream new line

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -17,12 +17,20 @@ public class MainStreams {
     }
 
     private static int minValue(int[] values) {
-        return IntStream.of(values).sorted().distinct().reduce(0, (x, y) -> x * 10 + y);
+        return IntStream.of(values)
+                .sorted()
+                .distinct()
+                .reduce(0, (x, y) -> x * 10 + y);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        Integer sum = integers.stream().reduce((s1, s2) -> s1 + s2).orElse(0);
-        Map<Boolean, List<Integer>> map = integers.stream().collect(Collectors.partitioningBy((p) -> (p % 2) == 0));
+        Integer sum = integers
+                .stream()
+                .reduce((s1, s2) -> s1 + s2)
+                .orElse(0);
+        Map<Boolean, List<Integer>> map = integers
+                .stream()
+                .collect(Collectors.partitioningBy((p) -> (p % 2) == 0));
         return map.get((sum % 2) != 0);
     }
 
